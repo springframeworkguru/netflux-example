f7adcfd03f6ed4dd1f4ece8bfa0b622fb416968c
John Thompson <john@springframework.guru>
2017-09-01 15:27:47 -0400

    Initial Project Add

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring NetFlux Example Project
+Example Reactive Programming project using Spring Framework 5. Shamlessly inspired by John Long's presentations.
---
+++ src/main/java/guru/springframework/netfluxexample/NetfluxExampleApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.netfluxexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxExampleApplication.class, args);
+	}
+}
