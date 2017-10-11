213355e774f38ba3e7682063a555af8f7a915f95
John Thompson <john@springframework.guru>
2017-10-11 12:39:12 -0400

    fixing errors to stream events

--- src/main/java/guru/springframework/netfluxexample/controllers/MovieController.java
+++ src/main/java/guru/springframework/netfluxexample/controllers/MovieController.java
@@ -26,7 +26,7 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping(value = "/{id}/events")
+    @GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<MovieEvent> streamMovieEvents(@PathVariable String id){
         return movieService.events(id);
     }
--- src/main/java/guru/springframework/netfluxexample/domain/Movie.java
+++ src/main/java/guru/springframework/netfluxexample/domain/Movie.java
@@ -1,9 +1,6 @@
 package guru.springframework.netfluxexample.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
@@ -12,7 +9,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Movie {
 
     private String id;
