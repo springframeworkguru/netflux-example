06431cacd3af1ac4fc03d16a39ac1bca3a829647
springframeworkguru <john@springframework.guru>
2018-03-31 11:14:24 +0530

    Changed parameter ordering in Movie constructor call.

--- src/main/java/guru/springframework/netfluxexample/bootstrap/BootstrapCLR.java
+++ src/main/java/guru/springframework/netfluxexample/bootstrap/BootstrapCLR.java
@@ -27,7 +27,7 @@ public class BootstrapCLR implements CommandLineRunner {
                 .thenMany(
                 Flux.just("Silence of the Lambdas", "AEon Flux", "Enter the Mono<Void>", "The Fluxxinator",
                         "Back to the Future", "Meet the Fluxes", "Lord of the Fluxes")
-                        .map(title -> new Movie(title, UUID.randomUUID().toString()))
+                        .map(title -> new Movie(UUID.randomUUID().toString(),title))
                         .flatMap(movieRepository::save))
                         .subscribe(null, null, () -> {
                             movieRepository.findAll().subscribe(System.out::println);
